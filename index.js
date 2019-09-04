81cc0506513e80946b82c677f1ab1efe4e69cbf6
Subrata Aich <subraich@in.ibm.com>
2019-09-04 15:32:01 +0530

    Changes done in Handler and Stack

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var time = require('time'); 
 exports.handler = (event, context, callback) => { 
     var currentTime = new time.Date();  
-    currentTime.setTimezone("India/Kolkata"); 
+    currentTime.setTimezone("America/New_York"); 
     callback(null, { 
         statusCode: '200', 
-        body: 'The time in Kolkata is: ' + currentTime.toString(), 
+        body: 'The time in New York is: ' + currentTime.toString(), 
     }); 
 }; 
