20ec4e42fc63a043d739ccefe4842a9fffc1323d
Subrata Aich <subraich@in.ibm.com>
2019-09-04 13:03:26 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var time = require('time'); 
+exports.handler = (event, context, callback) => { 
+    var currentTime = new time.Date();  
+    currentTime.setTimezone("America/New_York"); 
+    callback(null, { 
+        statusCode: '200', 
+        body: 'The time in New York is: ' + currentTime.toString(), 
+    }); 
+}; 
