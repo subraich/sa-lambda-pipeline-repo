bb1779ab9bfed12ffdd3bca1c926ad39e9fb948f
Subrata Aich <subraich@in.ibm.com>
2019-09-04 14:37:58 +0530

    Los Angeles -> Kolkata

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var time = require('time'); 
 exports.handler = (event, context, callback) => { 
     var currentTime = new time.Date();  
-    currentTime.setTimezone("America/Los_Angeles"); 
+    currentTime.setTimezone("India/Kolkata"); 
     callback(null, { 
         statusCode: '200', 
-        body: 'The time in Los Angeles is: ' + currentTime.toString(), 
+        body: 'The time in Kolkata is: ' + currentTime.toString(), 
     }); 
 }; 
