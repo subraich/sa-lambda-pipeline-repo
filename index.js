be3951b93073cbce9a1e124e79abad50ef2524d6
Subrata Aich <subraich@in.ibm.com>
2019-09-04 18:47:49 +0530

    NW->LA

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var time = require('time'); 
 exports.handler = (event, context, callback) => { 
     var currentTime = new time.Date();  
-    currentTime.setTimezone("America/New_York"); 
+    currentTime.setTimezone("America/Los Angeles"); 
     callback(null, { 
         statusCode: '200', 
-        body: 'The time in New York is: ' + currentTime.toString(), 
+        body: 'The time in Los Angeles is: ' + currentTime.toString(), 
     }); 
 }; 
